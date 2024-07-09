17f68d48692dbd931dac2126748f03e90b980008
robro <433665+robro@users.noreply.github.com>
2024-07-08 18:22:16 -0700

    Make word flash

--- ui/partial_word.gd
+++ ui/partial_word.gd
@@ -1,7 +1,20 @@
 extends Label
 
+@export var flash_speed := 0.075
+
+var finished_timer := Timer.new()
+var failed_timer := Timer.new()
+var flashing : bool = false
+
+
 func _ready():
 	Palette.connect("palette_change", _on_palette_change)
+	finished_timer.wait_time = flash_speed
+	failed_timer.wait_time = flash_speed
+	finished_timer.timeout.connect(_on_finished_timer_timeout)
+	failed_timer.timeout.connect(_on_failed_timer_timeout)
+	add_child(finished_timer)
+	add_child(failed_timer)
 
 
 func _process(_delta):
@@ -10,3 +23,35 @@ func _process(_delta):
 
 func _on_palette_change():
 	add_theme_color_override("font_color", Palette.colors[Palette.Type.PRIMARY])
+
+
+func _on_finished_timer_timeout():
+	flashing = false if flashing else true
+	var color = Palette.Type.HIGHLIGHT if flashing else Palette.Type.BACKGROUND
+	add_theme_color_override("font_color", Palette.colors[color])
+
+
+func _on_failed_timer_timeout():
+	flashing = false if flashing else true
+	var color = Palette.Type.BACKGROUND if flashing else Palette.Type.SECONDARY
+	add_theme_color_override("font_color", Palette.colors[color])
+
+
+func _on_word_finished_state_entered():
+	flashing = true
+	add_theme_color_override("font_color", Palette.colors[Palette.Type.HIGHLIGHT])
+	finished_timer.start()
+
+
+func _on_word_finished_state_exited():
+	finished_timer.stop()
+
+
+func _on_word_failed_state_entered():
+	flashing = true
+	add_theme_color_override("font_color", Palette.colors[Palette.Type.SECONDARY])
+	failed_timer.start()
+
+
+func _on_word_failed_state_exited():
+	failed_timer.stop()
