8a2a6ec236116bf8075fd3e249c69bffef44f5ef
robro <433665+robro@users.noreply.github.com>
2024-07-10 22:31:50 -0700

    Add mine spawner to increase challenge

--- object/food/food_spawner.gd
+++ object/food/food_spawner.gd
@@ -2,13 +2,7 @@ class_name FoodSpawner
 extends Node2D
 
 @export var inedible_time : float = 0.5
-@export_enum(
-	"primary",
-	"secondary",
-	"background",
-	"highlight",
-	"shadow"
-) var color : int
+@export var _color := Colors.Type.PRIMARY
 
 
 func spawn_food(text: String, grid: Grid) -> void:
@@ -18,7 +12,7 @@ func spawn_food(text: String, grid: Grid) -> void:
 			print("no free space")
 			return
 
-		add_child(Food.new(pos, char_, color, inedible_time))
+		add_child(Food.new(pos, char_, _color, inedible_time))
 
 
 func clear() -> void:
--- scene/gameplay/gameplay.gd
+++ scene/gameplay/gameplay.gd
@@ -4,18 +4,24 @@ extends Node
 @export var _grid : Grid
 @export var _snake : Snake
 @export var _food_spawner : FoodSpawner
+@export var _mine_spawner : FoodSpawner
 @export var _state_chart : StateChart
 @export var _small_splash : SplashProperties
 @export var _big_splash : SplashProperties
+@export var _mine_interval : float = 3.0
 
-var _go_timer := Timer.new()
+var _mine_timer := Timer.new()
+var _gameover_timer := Timer.new()
 var _word_idx : int = 0
 
 
 func _ready() -> void:
-	_go_timer.wait_time = 0.5
-	_go_timer.one_shot = true
-	add_child(_go_timer)
+	_gameover_timer.wait_time = 0.5
+	_gameover_timer.one_shot = true
+	add_child(_gameover_timer)
+	_mine_timer.one_shot = true
+	_mine_timer.connect("timeout", _on_mine_timer_timeout)
+	add_child(_mine_timer)
 	Words.words.shuffle()
 
 
@@ -26,6 +32,8 @@ func _on_seeking_state_entered() -> void:
 	_food_spawner.spawn_food(Global.target_word, _grid)
 	_word_idx += 1
 	_word_idx %= Words.words.size()
+	_mine_timer.wait_time = _mine_interval
+	_mine_timer.start()
 
 
 func add_points() -> void:
@@ -63,6 +71,8 @@ func _on_snake_collided() -> void:
 
 func _on_word_failed_state_entered() -> void:
 	_food_spawner.clear()
+	_mine_spawner.clear()
+	_mine_timer.stop()
 	Global.target_word = "".rpad(Global.target_word.length())
 	Global.multiplier = 1
 	Global.combo = 0
@@ -77,6 +87,8 @@ func _on_word_failed_state_exited() -> void:
 
 func _on_word_finished_state_entered() -> void:
 	_food_spawner.clear()
+	_mine_spawner.clear()
+	_mine_timer.stop()
 	Global.target_word = "".rpad(Global.target_word.length())
 	Global.multiplier = min(Global.multiplier * 2, Global.max_multiplier)
 	add_points()
@@ -90,13 +102,14 @@ func _on_word_finished_state_exited() -> void:
 
 
 func _on_game_over_state_entered() -> void:
+	_mine_timer.stop()
 	for node in get_tree().get_nodes_in_group("emitters"):
 		node.queue_free()
 
 	_snake._alive = false
 	Global.target_word = ""
 	Global.partial_word = "udied"
-	_go_timer.start()
+	_gameover_timer.start()
 
 
 func _on_game_over_state_processing(_delta: float) -> void:
@@ -105,9 +118,25 @@ func _on_game_over_state_processing(_delta: float) -> void:
 
 
 func _on_game_over_state_input(event: InputEvent) -> void:
-	if event is InputEventKey and _go_timer.time_left == 0:
+	if event is InputEventKey and _gameover_timer.time_left == 0:
 		Global.score = 0
 		Global.combo = 0
 		Global.max_combo = 0
 		Global.multiplier = 1
 		get_tree().reload_current_scene()
+
+
+func _on_mine_timer_timeout() -> void:
+	_mine_timer.wait_time *= 0.9
+	_mine_timer.start()
+	_mine_spawner.spawn_food(get_nonword_char(Global.target_word), _grid)
+
+
+func get_nonword_char(word: String) -> String:
+	var ascii_alpha := range(97, 97 + 26)
+	for num in word.to_ascii_buffer():
+		var idx := ascii_alpha.find(num)
+		if idx >= 0:
+			ascii_alpha.remove_at(idx)
+
+	return char(ascii_alpha[randi_range(0, ascii_alpha.size() - 1)])
